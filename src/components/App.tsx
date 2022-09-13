0cea2f0e28a1f09771057be3365073872a5372c1
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-13 14:32:53 -0700

    fix errors

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
     </div>
   );
 }
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,12 @@
 interface Props {
   title: string;
-  posterUrl: string;
+  posterSrc: string;
   genres: string[];
 }
 
 function MovieCard({
   title,
-  posterUrl = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }: Props) {
   return (
