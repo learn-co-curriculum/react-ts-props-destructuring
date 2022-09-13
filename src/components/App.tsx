e298c07ee7a0fd6adc8bde0cbdc384741bde2a29
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-13 14:30:15 -0700

    configure with ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  title: string;
+  posterUrl: string;
+  genres: string[];
+}
+
+function MovieCard({
+  title,
+  posterUrl = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+  genres,
+}: Props) {
+  return (
+    <div className="movie-card">
+      <img src={posterSrc} alt={title} />
+      <h2>{title}</h2>
+      <small>{genres.join(", ")}</small>
+    </div>
+  );
+}
+
+export default MovieCard;
